3a334f86e5f0a33d5cb85c1145d66578aa2b4325
smd-abdul-rahman <118162235+a-rahman-ali@users.noreply.github.com>
2023-11-05 18:58:15 +0000

    map-and-filter-chaining

---
+++ Iterations/07_loops.js
@@ -0,0 +1,21 @@
+// map and filter
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNumbers.map( (num) => { return num + 10})
+// console.log(newNums);
+// [
+//     11, 12, 13, 14, 15,
+//     16, 17, 18, 19, 20
+// ]
+
+const newNums = myNumbers
+                .map((num) => num * 10 ) // multiply each num by 10
+                .map( (num) => num + 1)  // increment each num by 1
+                .filter( (num) => num >= 40) // filter-out the num >= 40
+
+console.log(newNums);
+// [
+//     41, 51,  61, 71,
+//     81, 91, 101
+// ]
