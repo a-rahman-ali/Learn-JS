af9b3d6608198096a1a5c91c41ce9157a39905fa
smd-abdul-rahman <118162235+a-rahman-ali@users.noreply.github.com>
2023-11-05 13:32:47 +0000

    basics-of-switch-case

---
+++ ControlFlow/switchCase.js
@@ -0,0 +1,36 @@
+// Switch-Case 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = "march"
+const month = "may";
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+// Whenever there is a match of any case, every thing below that case
+// will be executed, until the break statement is encountered
+// but the default block is not executed in such cases
+// default block only gets executed if there doesn't exist any case-block with match in case
