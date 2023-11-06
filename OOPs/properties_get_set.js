cde751c48dd4f9cab1e2aea8b8a040ba2580a892
smd-abdul-rahman <118162235+a-rahman-ali@users.noreply.github.com>
2023-11-06 19:43:11 +0000

    define-getters-setters-by-Object.defineProperty()

---
+++ OOPs/properties_get_set.js
@@ -0,0 +1,31 @@
+// Defining getters and setters through properties
+
+// This is an old approach when getters and setters weren't available
+// by making use of Object.defineProperty() function
+
+function User(email, password){
+    this._email = email;
+    this._password = password
+
+    Object.defineProperty(this, 'email', {
+        get: function(){
+            return this._email.toUpperCase()
+        },
+        set: function(value){
+            this._email = value
+        }
+    })
+    Object.defineProperty(this, 'password', {
+        get: function(){
+            return this._password.toUpperCase()
+        },
+        set: function(value){
+            this._password = value
+        }
+    })
+
+}
+
+const chai = new User("chai@chai.com", "chai")
+
+console.log(chai.email); // CHAI@CHAI.COM
