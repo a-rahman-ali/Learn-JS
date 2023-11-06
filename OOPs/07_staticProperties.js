da8a387bc126b29050d4d501d7c7c23057345908
smd-abdul-rahman <118162235+a-rahman-ali@users.noreply.github.com>
2023-11-06 17:39:07 +0000

    static-keyword-in-js

---
+++ OOPs/07_staticProperties.js
@@ -0,0 +1,29 @@
+// static properties
+
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const hitesh = new User("hitesh")
+// console.log(hitesh.createId()) // works if we remove static before createId()
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+iphone.logMe(); // Username: iphone
+// console.log(iphone.createId()); // works if we remove static before createId()
